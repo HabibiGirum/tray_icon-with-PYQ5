4e4f41f42ab9aeca3fd44a38229a5d6f2be7a4d8
Habtamu <habtamugirum0489@gmail.com>
2024-03-11 21:01:50 +0300

    change

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -5,29 +5,29 @@ set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 
 set(PROGRAM_FILES_DIR "Program Files")
 
-set(directory_name_list
-  "osquery"
-)
+# set(directory_name_list
+#   "osquery"
+# )
 
 set(file_name_list
     "osqueryi.exe"
 )
 
-foreach(directory_name ${directory_name_list})
-  install(
-    DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
-    DESTINATION "."
-    COMPONENT osquery
-  )
-endforeach()
+# foreach(directory_name ${directory_name_list})
+#   install(
+#     DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
+#     DESTINATION "."
+#     COMPONENT osquery
+#   )
+# endforeach()
 
-foreach(file_name ${file_name_list})
+
   install(
-    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${file_name}"
+    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryi.exe"
     DESTINATION "."
     COMPONENT osquery
   )
-endforeach()
+
 
 install(
   FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryd/osqueryd.exe"
