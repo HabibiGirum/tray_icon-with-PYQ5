f120103b85e73bd9190d469bc0397646081b7998
Habtamu <habtamugirum0489@gmail.com>
2024-03-11 17:52:25 +0300

    update cmake

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -1,22 +1,39 @@
+#
+# Copyright (c) 2014-present, The osquery authors
+#
+# This source code is licensed as defined by the LICENSE file found in the
+# root directory of this source tree.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+#
 
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
-# set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
+set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
 
-
-set(PROGRAM_FILES_DIR "Program Files")
+if ("${OSQUERY_BITNESS}" STREQUAL "32")
+  set(PROGRAM_FILES_DIR "Program Files (x86)")
+else()
+  set(PROGRAM_FILES_DIR "Program Files")
+endif()
 
 set(directory_name_list
-  "Vistar"
-  "osquery"
+  "certs"
+  "packs"
+  "log"
 )
 
 set(file_name_list
-  "osqueryd.exe"
+  "manage-osqueryd.ps1"
+  "osquery.conf"
+  "osquery.flags"
+  "osquery.man"
+  "osquery_utils.ps1"
+  "osqueryi.exe"
 )
 
 foreach(directory_name ${directory_name_list})
   install(
-    DIRECTORY "${OSQUERY_DATA_PATH}/Program Files/osquery/${directory_name}"
+    DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
     DESTINATION "."
     COMPONENT osquery
   )
@@ -24,20 +41,14 @@ endforeach()
 
 foreach(file_name ${file_name_list})
   install(
-    FILES "${OSQUERY_DATA_PATH}/Program Files/osquery/${file_name}"
+    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${file_name}"
     DESTINATION "."
     COMPONENT osquery
   )
 endforeach()
 
-# install(
-#   FILES "${OSQUERY_DATA_PATH}/Program Files/osquery/osqueryd/osqueryd.exe"
-#   DESTINATION "osqueryd"
-#   COMPONENT osquery
-# )
-
 install(
-  FILES "${OSQUERY_DATA_PATH}/Program Files/osquery/osqueryd/osqueryd.exe"
+  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryd/osqueryd.exe"
   DESTINATION "osqueryd"
   COMPONENT osquery
 )
