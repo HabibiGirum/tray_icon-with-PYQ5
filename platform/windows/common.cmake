767692ffa4c0a387a8c2b8b254628a685662479e
Habtamu <habtamugirum0489@gmail.com>
2024-03-13 17:52:41 +0300

    cmake

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -1,42 +1,16 @@
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
 
-if ("${OSQUERY_BITNESS}" STREQUAL "32")
-  set(PROGRAM_FILES_DIR "Program Files (x86)")
-else()
-  set(PROGRAM_FILES_DIR "Program Files")
-endif()
 
-set(directory_name_list
-  "certs"
-  "packs"
-  
+set(PROGRAM_FILES_DIR "Program Files")
+
+
+
+install(
+  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryi.exe"
+  DESTINATION "."
+  COMPONENT osquery
 )
-
-set(file_name_list
-  "manage-osqueryd.ps1"
-  "osquery.conf"
-  "osquery.flags"
-  "osquery.man"
-  "osquery_utils.ps1"
-  "osqueryi.exe"
-)
-
-foreach(directory_name ${directory_name_list})
-  install(
-    DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
-    DESTINATION "."
-    COMPONENT osquery
-  )
-endforeach()
-
-foreach(file_name ${file_name_list})
-  install(
-    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${file_name}"
-    DESTINATION "."
-    COMPONENT osquery
-  )
-endforeach()
 
 
 install(
