0836bdcc321f470ee0d75d46342a6bb70cd26539
Habtamu <habtamugirum0489@gmail.com>
2024-03-13 18:15:04 +0300

    CMAKE BUG

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -1,11 +1,6 @@
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
-
-
 set(PROGRAM_FILES_DIR "Program Files")
-
-
-
 install(
   FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryi.exe"
   DESTINATION "."
@@ -18,15 +13,3 @@ install(
   DESTINATION "osqueryd"
   COMPONENT osquery
 )
-
-# set(VISTAR_EXE_PATH "${CMAKE_SOURCE_DIR}/dist/vistar.exe")
-
-# if(EXISTS "${VISTAR_EXE_PATH}")
-#   install(
-#     FILES "${VISTAR_EXE_PATH}"
-#     DESTINATION "vistar"  
-#     COMPONENT osquery
-#   )
-# else()
-#   message(WARNING "File not found: ${VISTAR_EXE_PATH}. Skipping installation.")
-# endif()
