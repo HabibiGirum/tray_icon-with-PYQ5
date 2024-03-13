2815683d0b87ced6bf357ff0275f401cc4b7fbde
Habtamu <habtamugirum0489@gmail.com>
2024-03-13 14:46:51 +0300

    update

--- platform/windows/wix.cmake
+++ platform/windows/wix.cmake
@@ -7,15 +7,12 @@
 # SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
 #
 
-if(OSQUERY_BITNESS STREQUAL "32")
-  set(CPACK_WIX_SIZEOF_VOID_P "4")
 
-elseif(OSQUERY_BITNESS STREQUAL "64")
-  set(CPACK_WIX_SIZEOF_VOID_P "8")
 
-else()
-  message(FATAL_ERROR "The OSQUERY_BITNESS variable must be set to either 32 or 64 according to the build type")
-endif()
+
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+
 
 set(CPACK_WIX_PRODUCT_ICON "${CMAKE_SOURCE_DIR}/images/vistar.ico")
 set(CPACK_WIX_UPGRADE_GUID "3f59e9c0-720f-4948-afa9-883805397642")
