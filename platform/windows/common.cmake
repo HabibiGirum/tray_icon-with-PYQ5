ca5b0f4c4ca0ebb52b2595d3d5e0cc9aec560bac
Habtamu <habtamugirum0489@gmail.com>
2024-03-14 19:58:30 +0300

    update now

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -1,15 +1,54 @@
+#
+# Copyright (c) 2014-present, The osquery authors
+#
+# This source code is licensed as defined by the LICENSE file found in the
+# root directory of this source tree.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+#
+
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
-set(OSQUERY_BITNESS "64")
-set(PROGRAM_FILES_DIR "Program Files")
-install(
-  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryi.exe"
-  DESTINATION "."
-  COMPONENT vistar
+set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
+
+if ("${OSQUERY_BITNESS}" STREQUAL "32")
+  set(PROGRAM_FILES_DIR "Program Files (x86)")
+else()
+  set(PROGRAM_FILES_DIR "Program Files")
+endif()
+
+set(directory_name_list
+  "certs"
+  "packs"
+  "log"
 )
 
+set(file_name_list
+  "manage-osqueryd.ps1"
+  "osquery.conf"
+  "osquery.flags"
+  "osquery.man"
+  "osquery_utils.ps1"
+  "osqueryi.exe"
+)
+
+foreach(directory_name ${directory_name_list})
+  install(
+    DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
+    DESTINATION "."
+    COMPONENT osquery
+  )
+endforeach()
+
+foreach(file_name ${file_name_list})
+  install(
+    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${file_name}"
+    DESTINATION "."
+    COMPONENT osquery
+  )
+endforeach()
 
 install(
-  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/vistar/vistar.exe"
-  DESTINATION "vistar"
-  COMPONENT vistar
+  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryd/osqueryd.exe"
+  DESTINATION "osqueryd"
+  COMPONENT osquery
 )
--- platform/windows/wix.cmake
+++ platform/windows/wix.cmake
@@ -1,11 +1,24 @@
+#
+# Copyright (c) 2014-present, The osquery authors
+#
+# This source code is licensed as defined by the LICENSE file found in the
+# root directory of this source tree.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+#
 
+if(OSQUERY_BITNESS STREQUAL "32")
+  set(CPACK_WIX_SIZEOF_VOID_P "4")
 
-set(CPACK_WIX_SIZEOF_VOID_P "8")
+elseif(OSQUERY_BITNESS STREQUAL "64")
+  set(CPACK_WIX_SIZEOF_VOID_P "8")
 
+else()
+  message(FATAL_ERROR "The OSQUERY_BITNESS variable must be set to either 32 or 64 according to the build type")
+endif()
 
-
-set(CPACK_WIX_PRODUCT_ICON "${CMAKE_SOURCE_DIR}/images/vistar.ico")
-set(CPACK_WIX_UPGRADE_GUID "3f59e9c0-720f-4948-afa9-883805397642")
-set(CPACK_WIX_PATCH_FILE "${CMAKE_SOURCE_DIR}/osquery_wix_patch.xml")
+set(CPACK_WIX_PRODUCT_ICON "${OSQUERY_DATA_PATH}/control/osquery.ico")
+set(CPACK_WIX_UPGRADE_GUID "ea6c7327-461e-4033-847c-acdf2b85dede")
+set(CPACK_WIX_PATCH_FILE "${OSQUERY_DATA_PATH}/control/msi/osquery_wix_patch.xml")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "osquery")
 set(CPACK_WIX_EXTENSIONS "WixUtilExtension")
